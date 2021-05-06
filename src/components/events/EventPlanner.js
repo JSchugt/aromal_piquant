c43518ba6f9c3aaf1ba00e98bd3a99072209b18e
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-05-05 23:33:30 -0500

    Sort Events with future and historic being shown

--- src/components/events/EventPlanner.js
+++ src/components/events/EventPlanner.js
@@ -3,31 +3,54 @@ import { Link, useHistory } from "react-router-dom"
 import { getEventsByUserId } from "../../modules/eventsManager"
 import { EventCard } from "./EventCard"
 import "./events.css"
-import {userStorageKey} from "../auth/authSettings"
+import { userStorageKey } from "../auth/authSettings"
 
 export const EventPlanner = () => {
     const [events, setEvents] = useState([])
+    const [historicEvents, setHistoricEvents] = useState([])
     const history = useHistory()
     const getSoonest = (eventsToBeSorted) => {
+        const now = new Date()
+
         let sorted = eventsToBeSorted.sort(
             (currentEntry, nextEntry) =>
                 Date.parse(currentEntry.eventDate) - Date.parse(nextEntry.eventDate)
         )
-        return [...sorted]
+        let future = []
+        let completed = []
+        for (let i = 0; i < sorted.length; i++) {
+            if (Date.parse(sorted[i].eventDate) > (Date.now() - 86400000).toFixed(0)) {
+                future.push(sorted[i])
+            } else {
+                completed.push(sorted[i])
+            }
+        }
+        setHistoricEvents([...completed])
+        setEvents([...future])
+        // return [...sorted]
     }
-    useEffect(()=>{
+    useEffect(() => {
         getEventsByUserId(sessionStorage.getItem(userStorageKey))
-        .then(responseFromApi => {setEvents(getSoonest(responseFromApi))})
-    },[])
-    const handleEventOnClick =(evt)=>{
+            .then(responseFromApi => { getSoonest(responseFromApi) })
+    }, [])
+    const handleEventOnClick = (evt) => {
         evt.preventDefault()
         history.push(`/events/${evt.target.id}`)
     }
     return (
         <>
-        {events.map(evt => {
-            return <div  onClick={handleEventOnClick} className="eventDatePlanner"><h2 id={evt.id}>{evt.eventDate}</h2></div>
-        })}
+            <div className="allEvents">
+                <div>
+                    {events.map(evt => {
+                        return <div onClick={handleEventOnClick} className="eventDatePlanner"><h2 id={evt.id}>{evt.eventDate}</h2></div>
+                    })}
+                </div>
+                <div>
+                {historicEvents.map(evt3 => {
+                        return <div onClick={handleEventOnClick} className="eventDatePlanner"><h2 id={evt3.id}>{evt3.eventDate}</h2></div>
+                    })}
+                </div>
+            </div>
             <Link to={"/events/create"}>
                 <button>Add Event</button>
             </Link>
